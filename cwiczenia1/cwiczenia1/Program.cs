bdc44693a22470f5473e0380d8e8208d6f09fe82
Joanna Piętak <s16596@pjwstk.edu.pl>
2020-02-29 18:03:25 +0100

    modyfikacje

--- cwiczenia1/cwiczenia1/Program.cs
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace cwiczenia1
@@ -8,12 +9,23 @@ namespace cwiczenia1
     {
         public static async Task Main(string[] args)
         {
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
             var client = new HttpClient();
-            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+            HttpResponseMessage result = await client.GetAsync(url);
 
             if (result.IsSuccessStatusCode)
             {
                 var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+
+
             }
 
         }
