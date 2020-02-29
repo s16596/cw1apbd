7af65dd55a3d07e4cd80b30447a8f7d57cb092f6
Joanna Piętak <s16596@pjwstk.edu.pl>
2020-02-29 17:47:25 +0100

    dodanie projektu

---
+++ cwiczenia1/cwiczenia1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace cwiczenia1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var html = await result.Content.ReadAsStringAsync();
+            }
+
+        }
+    }
+}
